a4c2fccf9db480e10aad49a587db34f0ae907180
JoacoErosa <joaquin.erosa@lightit.io>
2026-05-07 11:36:31 -0300

    feat: structur changes

--- src/Clinics/Domain/Models/Clinic.php
+++ src/Clinics/Domain/Models/Clinic.php
@@ -4,19 +4,23 @@ declare(strict_types=1);
 
 namespace Lightit\Clinics\Domain\Models;
 
-use Lightit\Doctors\Domain\Models\Doctor;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Lightit\Doctors\Domain\Models\Doctor;
 
 class Clinic extends Model
 {
+    #[\Override]
     protected $fillable = [
         'name',
         'address',
     ];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany<\Lightit\Doctors\Domain\Models\Doctor, $this, \Illuminate\Database\Eloquent\Relations\Pivot>
+     */
     public function doctors(): BelongsToMany
-     {
-         return $this->belongsToMany(Doctor::class);
-     }
+    {
+        return $this->belongsToMany(Doctor::class);
+    }
 }
--- src/Doctors/Domain/Models/Doctor.php
+++ src/Doctors/Domain/Models/Doctor.php
@@ -4,17 +4,21 @@ declare(strict_types=1);
 
 namespace Lightit\Doctors\Domain\Models;
 
-use Lightit\Clinics\Domain\Models\Clinic;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Lightit\Clinics\Domain\Models\Clinic;
 
 class Doctor extends Model
 {
+    #[\Override]
     protected $fillable = [
         'first_name',
         'last_name',
     ];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany<\Lightit\Clinics\Domain\Models\Clinic, $this, \Illuminate\Database\Eloquent\Relations\Pivot>
+     */
     public function clinics(): BelongsToMany
     {
         return $this->belongsToMany(Clinic::class);
