c1f4d0f04836155fe99e47d54ee30385419fb443
JoacoErosa <joaquin.erosa@lightit.io>
2026-05-12 15:56:04 -0300

    fix: removed unused token for JWT, changed to use deleteOrFail, changed query to avoid N+1, added whenLoaded

--- database/migrations/2026_05_07_143430_create_patients_table.php
+++ database/migrations/2026_05_07_143430_create_patients_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
         });
