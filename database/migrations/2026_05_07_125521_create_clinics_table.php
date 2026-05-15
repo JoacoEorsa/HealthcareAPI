16fb2959b1345296ae8d25c2c6bf97d4f5370692
JoacoErosa <joaquin.erosa@lightit.io>
2026-05-15 16:32:37 -0300

    fix: changed to use softDelets to avoid foreignKey violations

--- database/migrations/2026_05_07_125521_create_clinics_table.php
+++ database/migrations/2026_05_07_125521_create_clinics_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('address');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
+            $table->softDeletes();
         });
     }
 
--- src/Clinics/Domain/Actions/DeleteClinicAction.php
+++ src/Clinics/Domain/Actions/DeleteClinicAction.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace Lightit\Clinics\Domain\Actions;
 
+use Lightit\Appointments\Domain\Enums\AppointmentStatus;
 use Lightit\Clinics\Domain\Models\Clinic;
 
 class DeleteClinicAction
 {
     public function execute(Clinic $clinic): void
     {
+        $clinic->appointments()->where('status', AppointmentStatus::Scheduled)
+            ->update(['status' => AppointmentStatus::Cancelled]);
         $clinic->doctors()->detach();
 
         $clinic->deleteOrFail();
