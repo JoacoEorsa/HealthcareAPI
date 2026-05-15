067a0958132a58c80a792503da555635df6ec6d9
JoacoErosa <joaquin.erosa@lightit.io>
2026-05-15 16:34:54 -0300

    fix: changed to use softDelets to avoid foreign key violations

--- database/migrations/2026_05_07_130938_create_doctors_table.php
+++ database/migrations/2026_05_07_130938_create_doctors_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('last_name');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
+            $table->softDeletes();
         });
     }
 
